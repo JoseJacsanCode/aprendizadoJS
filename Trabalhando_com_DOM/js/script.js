70796e7ea9fefd905d0a380b0c0f7ca7f2418996
Jose Jacsan <jjacsan@gmail.com>
2026-07-07 15:23:11 -0300

    Trabalhando com DOM

---
+++ Trabalhando_com_DOM/js/script.js
@@ -0,0 +1,10 @@
+var p1 = document.getElementsByTagName('p')[1];
+var corpo = document.body;
+// var d = document.getElementById('msg');
+// document.write(p1.innerHTML);
+// d.style.background = 'green';
+// d.innerText = 'Estou aguardando...'
+// var d = document.getElementsByName('msg')[0];
+// d.innerText = 'Olá...';
+var d = document.querySelector('.msg');
+d.style.background = 'blue';
