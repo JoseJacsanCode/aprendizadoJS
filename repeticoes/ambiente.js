235236ca0003a8c387f665668d431f7be1de533c
Jose Jacsan <jjacsan@gmail.com>
2026-08-06 14:22:24 -0300

    Commit backup

---
+++ repeticoes/ambiente.js
@@ -0,0 +1,28 @@
+// laço do-while
+
+var c = 1;
+do {
+    console.log(`Passo ${c}`);
+    c++
+} while (c <= 6);
+
+
+
+//laço while
+
+// var c = 1;
+// while (c <= 3) {
+//     console.log(`${c} - Tudo bem?`);
+//     c++;
+// }
+
+
+
+
+// console.log('Tudo bem?');
+// console.log('Tudo bem?');
+// console.log('Tudo bem?');
+// console.log('Tudo bem?');
+// console.log('Tudo bem?');
+// console.log('Tudo bem?');
+
